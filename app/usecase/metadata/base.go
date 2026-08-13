420d62b5e1f773a5e6576e60a503b3c35b041988
Ranggalawe Istifajar <51109081+gatsu420@users.noreply.github.com>
2026-08-13 07:29:27 +0700

    add usecase for adding tool

--- app/usecase/metadata/base.go
+++ app/usecase/metadata/base.go
@@ -10,6 +10,7 @@ type Usecase interface {
 	InsertUser(ctx context.Context, args InsertUserArgs) (InsertUserResult, error)
 	InsertUserToken(ctx context.Context, args InsertUserTokenArgs) error
 	GetUserToken(ctx context.Context, args GetUserTokenArgs) (GetUserTokenResult, error)
+	AddTool(ctx context.Context, args AddToolArgs) error
 }
 
 type usecaseImpl struct {
--- app/usecase/metadata/metadata.go
+++ app/usecase/metadata/metadata.go
@@ -2,6 +2,7 @@ package metadata
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/gatsu420/kisu-be/app/repository/pgrepo"
 	"golang.org/x/oauth2"
@@ -64,4 +65,55 @@ func (u *usecaseImpl) GetUserToken(ctx context.Context, args GetUserTokenArgs) (
 	return GetUserTokenResult{
 		Token: result.Token,
 	}, nil
+}
+
+type AddToolColumn struct {
+	Name        string
+	Type        string
+	Description string
+	IsSelected  bool
+}
+
+type AddToolQueryExample struct {
+	Description string
+	Query       string
+}
+
+type AddToolArgs struct {
+	ToolDescription string
+	TableName       string
+	Columns         []AddToolColumn
+	QueryExamples   []AddToolQueryExample
+}
+
+func (u *usecaseImpl) AddTool(ctx context.Context, args AddToolArgs) error {
+	columns := []pgrepo.AddToolColumn{}
+	for _, c := range args.Columns {
+		columns = append(columns, pgrepo.AddToolColumn{
+			Name:        c.Name,
+			Type:        c.Type,
+			Description: c.Description,
+			IsSelected:  c.IsSelected,
+		})
+	}
+
+	queryExamples := []pgrepo.AddToolQueryExample{}
+	for _, qe := range args.QueryExamples {
+		queryExamples = append(queryExamples, pgrepo.AddToolQueryExample{
+			Description: qe.Description,
+			Query:       qe.Query,
+		})
+	}
+
+	err := u.pgRepo.AddTool(ctx, pgrepo.AddToolArgs{
+		ToolDescription: args.ToolDescription,
+		TableName:       args.TableName,
+		Columns:         columns,
+		QueryExamples:   queryExamples,
+	})
+	if err != nil {
+		return fmt.Errorf("unable to add tool: %w", err)
+	}
+
+	return nil
 }
