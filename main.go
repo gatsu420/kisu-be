3f3e14a957aea3c413423b4803653b6ec9fd4221
Ranggalawe Istifajar <51109081+gatsu420@users.noreply.github.com>
2026-09-09 00:14:50 +0700

    tidy main.go structure

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"flag"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
@@ -28,69 +29,83 @@ import (
 )
 
 func main() {
-	envPath := flag.String("env-path", "", "path of env file")
-	flag.Parse()
-	config, err := commonconfig.NewConfig(*envPath)
+	err := runServer()
 	if err != nil {
 		slog.Error(err.Error(),
 			slog.Int(commonerr.StatusCodeKey, http.StatusInternalServerError),
 			slog.Any(commonerr.ErrKey, err))
-	}
-
-	server := startServer(context.Background(), config)
-	quitCh := make(chan os.Signal, 1)
-	signal.Notify(quitCh, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		err := server.ListenAndServe()
-		if err != nil && !errors.Is(err, http.ErrServerClosed) {
-			slog.Error("unable to serve incoming connections",
-				slog.Any(commonerr.ErrKey, err),
-				slog.Int(commonerr.StatusCodeKey, http.StatusInternalServerError),
-			)
-		}
-	}()
-
-	<-quitCh
-	slog.Info("stopping http server")
-
-	timeoutCtx, timeoutCancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer timeoutCancel()
-
-	err = server.Shutdown(timeoutCtx)
-	if err != nil {
-		slog.Error("http server shutdown error",
-			slog.Any(commonerr.ErrKey, err),
-			slog.Int(commonerr.StatusCodeKey, http.StatusInternalServerError),
-		)
+		os.Exit(1)
 	}
 }
 
-func startServer(ctx context.Context, config commonconfig.Config) *http.Server {
-	googleAuth := googleauthadapter.NewAdapter(config.GoogleAuthClientID, config.GoogleAuthClientSecret, config.GoogleAuthRedirectURL)
-	bqRepo := bqrepo.NewRepository(config.ProjectID, googleAuth)
+func runServer() error {
+	envPath := flag.String("env-path", "", "path of env file")
+	flag.Parse()
 
+	config, err := commonconfig.NewConfig(*envPath)
+	if err != nil {
+		return fmt.Errorf("unable to load config: %w", err)
+	}
+
+	server, err := createServer(context.Background(), config)
+	if err != nil {
+		return err
+	}
+
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- server.ListenAndServe()
+	}()
+
+	quitCh := make(chan os.Signal, 1)
+	signal.Notify(quitCh, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case err := <-errCh:
+		if !errors.Is(err, http.ErrServerClosed) {
+			return fmt.Errorf("unable to serve incoming connections: %w", err)
+		}
+
+		return nil
+	case <-quitCh:
+		slog.Info("stopping http server")
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = server.Shutdown(shutdownCtx)
+	if err != nil {
+		return fmt.Errorf("http server shutdown: %w", err)
+	}
+
+	return nil
+}
+
+func createServer(ctx context.Context, config commonconfig.Config) (*http.Server, error) {
+	googleAuth := googleauthadapter.NewAdapter(
+		config.GoogleAuthClientID,
+		config.GoogleAuthClientSecret,
+		config.GoogleAuthRedirectURL,
+	)
+
+	bqRepo := bqrepo.NewRepository(config.ProjectID, googleAuth)
 	pgPool, err := pgxpool.New(ctx, config.PostgresDSN)
 	if err != nil {
-		slog.Error("unable to create postgres connection pool",
-			slog.Int(commonerr.StatusCodeKey, http.StatusInternalServerError),
-			slog.Any(commonerr.ErrKey, err))
+		return nil, fmt.Errorf("unable to create postgres connection pool: %w", err)
 	}
 	pgRepo := pgrepo.NewRepository(pgPool)
+	stateRepo := staterepo.NewRepository()
 
 	genaiClient, err := genai.NewClient(ctx, &genai.ClientConfig{
 		APIKey: config.GeminiApiKey,
 	})
 	if err != nil {
-		slog.Error("unable to create genai client",
-			slog.Any(commonerr.ErrKey, err),
-			slog.Int(commonerr.StatusCodeKey, http.StatusInternalServerError),
-		)
+		return nil, fmt.Errorf("unable to create genai client: %w", err)
 	}
 
 	metadataUsecase := metadata.NewUsecase(pgRepo, bqRepo)
 	geminiAdapter := geminiadapter.NewAdapter(genaiClient, metadataUsecase)
-	stateRepo := staterepo.NewRepository()
-
 	answerUsecase := answer.NewUsecase(geminiAdapter)
 
 	authHandler := authhandlerv1.NewHandler(googleAuth, metadataUsecase, stateRepo)
@@ -99,14 +114,13 @@ func startServer(ctx context.Context, config commonconfig.Config) *http.Server {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /auth/v1/get-permission", authHandler.GetPermission)
 	mux.HandleFunc("GET /auth/v1/callback", authHandler.Callback)
-	authMiddleware := middleware.Chain(
-		middleware.RefreshToken(pgRepo, googleAuth),
-	)
-	mux.Handle("POST /answer/v1/tool", authMiddleware(http.HandlerFunc(answerHandler.AddTool)))
-	mux.Handle("GET /answer/v1/answer", authMiddleware(http.HandlerFunc(answerHandler.GetAnswer)))
+
+	refreshToken := middleware.RefreshToken(pgRepo, googleAuth)
+	mux.Handle("POST /answer/v1/tool", refreshToken(http.HandlerFunc(answerHandler.AddTool)))
+	mux.Handle("GET /answer/v1/answer", refreshToken(http.HandlerFunc(answerHandler.GetAnswer)))
 
 	return &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
-	}
+	}, nil
 }
